212ae343c6f90d776f8e2032147b91eeba32c1fe
admin <admin@DESKTOP-THLDMK9>
2018-12-11 13:49:02 +0800

    99999

--- module/web/src/main/java/com/web/test/TestController.java
+++ module/web/src/main/java/com/web/test/TestController.java
@@ -9,9 +9,13 @@ import com.entity.User;
 public class TestController {
   @RequestMapping("hello")
   public String helloWorld() {
+	  System.out.println("666666");
+	  System.out.println("333355");
 	  System.out.println("333333");
 	  User user = new User();
 	  System.out.println("000000");
+	  System.out.println("11111111");
+	  System.out.println("222222");
 	  System.out.println("222222");
 	  return "hello world";
   }
