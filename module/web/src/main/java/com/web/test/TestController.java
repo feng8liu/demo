96f551e22ea6d57973803cf39a3a0fa4c57f9c41
feng8liu <41537598+feng8liu@users.noreply.github.com>
2018-12-11 09:57:38 +0800

    Add files via upload

---
+++ module/web/src/main/java/com/web/test/TestController.java
@@ -0,0 +1,15 @@
+package com.web.test;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.entity.User;
+
+@RestController
+public class TestController {
+  @RequestMapping("hello")
+  public String helloWorld() {
+	  User user = new User();
+	  return "hello world";
+  }
+}
